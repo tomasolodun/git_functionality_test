d92691ecf423febc9b7607377c6b040e566d3133
Тамара Солодун <solodun.t@donnu.edu.ua>
2021-03-10 11:57:47 +0200

    hello gamma

--- TestGitFunctionality/TestGitFunctionality/Program.cs
+++ TestGitFunctionality/TestGitFunctionality/Program.cs
@@ -6,7 +6,7 @@ namespace TestGitFunctionality
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, master!");
+            Console.WriteLine("Hello, gamma!");
             Console.WriteLine("Hello, gamma!");
         }
     }
